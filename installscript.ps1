08dc3c804e585e4e2926f90c584819b43527bc9e
Henrik Motzkus <henrik.motzkus@live.com>
2021-09-24 07:43:19 +0200

    initial

---
+++ installscript.ps1
@@ -0,0 +1,2 @@
+# Install script that creates a Task (shudownscript.ps1) when the user shuts down the OS
+
---
+++ shutdown.ps1
@@ -0,0 +1,19 @@
+# Shutdown Script that is called by a task
+
+if ((Get-Module -ListAvailable -Name Az.Accounts) -and (Get-Module -ListAvailable -Name Az.Compute)) {
+    Stop-MySelf
+} 
+else {
+    install-module -name Az.Accounts
+    install-module -name Az.Compute
+    Stop-MySelf
+}
+
+function Stop-MySelf () {
+    Connect-AzAccount -Identity
+    $instance = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+    $name = $instance.compute.name
+    $rg = $instance.compute.resourceGroupNam
+    Stop-AzVM -Name $name -ResourceGroupName $rg
+
+}
