a86306d9598e8a0a7ee6821919175c8aa9b2e92e
Henrik Motzkus <hemotzku@microsoft.com>
2021-09-24 11:42:10 +0200

    test

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
-$fileUri = @("https://xxxxxxx.blob.core.windows.net/buildServer1/1_Add_Tools.ps1",
-"https://xxxxxxx.blob.core.windows.net/buildServer1/2_Add_Features.ps1",
-"https://xxxxxxx.blob.core.windows.net/buildServer1/3_CompleteInstall.ps1")
+Connect-AzAccount
+
+$fileUri = @("https://raw.githubusercontent.com/henrikmotzkus/CSE-AzureVMShutdown/main/installscript.ps1",
+"https://raw.githubusercontent.com/henrikmotzkus/CSE-AzureVMShutdown/main/shutdown.ps1")
 
 $settings = @{"fileUris" = $fileUri};
 
@@ -9,9 +10,9 @@ $storageKey = "1234ABCD"
 $protectedSettings = @{"storageAccountName" = $storageAcctName; "storageAccountKey" = $storageKey; "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File 1_Add_Tools.ps1"};
 
 #run command
-Set-AzVMExtension -ResourceGroupName <resourceGroupName> `
-    -Location <locationName> `
-    -VMName <vmName> `
+Set-AzVMExtension -ResourceGroupName "aadjoinedhostpool" `
+    -Location "westeurope" `
+    -VMName "vm-0" `
     -Name "buildserver1" `
     -Publisher "Microsoft.Compute" `
     -ExtensionType "CustomScriptExtension" `
